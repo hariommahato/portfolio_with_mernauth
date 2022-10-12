e1f3acfd7f32b7855847d468b93afc268ad12dc3
hariom <hariommahato20@gmail.com>
2022-10-12 16:11:57 +0545

    make it better

--- Server.js
+++ Server.js
@@ -54,8 +54,5 @@ const PORT =process.env.PORT || 3002
 
 if(process.env.NODE_ENV="production"){
     app.use(express.static("./frontend/build"))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname, 'frontend','build','index.html'));
-    })
 }
 app.listen(PORT,() => console.log("Server running on port " + PORT));
